9cbb767ea848648f0b583eb97d8a14b47124b88b
jrmkim50 <jrmkim50@gmail.com>
2021-01-30 19:18:31 -0500

    added home page

--- everybody-eats-app/src/components/home/Home.js
+++ everybody-eats-app/src/components/home/Home.js
@@ -1,9 +1,28 @@
 const Home = () => {
-
     return (
         <div>
+            <h1 style={{ textAlign: 'center', fontSize: 'xx-large', color: '#663300' }}>Making Sure Everybody Eats!</h1>
+            <p style={{ marginLeft: '25%', marginRight: '25%', fontSize: 'large', fontStyle: 'italic' }}>
+                <br></br> Thank you for taking the time to learn about our website! We are a group of students passionate about helping others find healthier food options.
+                    We feel that community gardens are some of the most underrated sources of fresh and nutritious foods.<br></br>
+            </p>
+            <p style={{ marginLeft: '25%', marginRight: '25%', fontSize: 'large' }}> <br></br>To find and learn about community gardens near you, please visit the "Map" tab. <br></br><br></br>To add a community garden to the map, please visit the "New" tab. We appreciate all contributions!</p>
+            <br />
+            <hr style={{ maxWidth: '80%', margin: 'auto' }}/>
+            <br />
+            <p style={{ marginLeft: '25%', marginRight: '25%', fontSize: 'x-large', color: '#663300' }} >There are many advantages to being involved with a community garden in your local area:<br></br> </p>
+            <p style={{ marginLeft: '25%', marginRight: '25%', fontSize: 'medium' }}>1. Many low-income communities don't have access to fresh produce, so community gardens can provide many residents with affordable, high quality ingredients they normally wouldn't have been able to obtain.
+                    <br></br><br></br> 2. The obvious health benefits of increased fruit and vegetable intake. <br></br><br></br> 3. Reduced risk of obesity and obesity-related diseases.<br></br> <br></br>4. Increased physical activity associated with setting up and maintaining community gardens.</p>
+            <br/>
+            <hr style={{ maxWidth: '80%', margin: 'auto' }}/>
+            <br/>
+            <p style={{ marginLeft: '25%', marginRight: '25%', fontSize: 'x-large', color: '#663300' }}>There are many environmental benefits as well: <br></br> </p>
+            <p style={{ marginLeft: '25%', marginRight: '25%', fontSize: 'medium' }}>1. An increase in local biodiversity <br></br> <br></br>2. Improves local air and soil quality <br></br> <br></br> 3. Can help combat global warming.</p>
+            <p style={{ marginLeft: '25%', marginRight: '25%', fontSize: 'medium' }}> <br></br> <br></br><br></br> Community gardens can also bring crime rates down and provide economic opportunities for local residents. To learn more about the benefits of Community gardens, please visit <a href="https://greenleafcommunities.org/the-many-benefits-of-community-gardens/npm star" >greenleafcommunities.org</a><br></br> <br></br><br></br></p>
         </div>
+
     );
 }
 
 export default Home;
+
